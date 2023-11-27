fab75c973abc8bcf8b2d6e0d7feab112b4373854
PetrusAriaa <petrusariacr25@gmail.com>
2023-11-27 11:35:02 +0700

    fix: fix airflow scheduling

--- dags/dag.py
+++ dags/dag.py
@@ -1,31 +1,25 @@
 import pendulum
 from datetime import datetime, timedelta
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
+from airflow.decorators import task, dag
 from stock_trend.start_stock_trend import Start
 
 local_tz = pendulum.timezone('Asia/Jakarta')
 
-# Define the default_args for your DAG
-default_args = {
-    'owner': 'airflow',
-    'start_date': datetime(2023, 11, 19, 19, 30, tzinfo=local_tz),
-    'retries': 1,
-    'catchup': False,
-    'retry_delay': timedelta(minutes=10),
-}
 
-# Define the DAG
-dag = DAG(
-    'my_spark_dag',
-    default_args=default_args,
-    description='A DAG to run a Spark job',
+@dag(
+    dag_id='rekdat-aria',
     schedule_interval='15 23 * * 5', # Set the schedule interval as needed
+    start_date=datetime(2022, 11, 19, 19, 30, tzinfo=local_tz),
+    catchup=False,
+    dagrun_timeout=timedelta(minutes=60),
 )
 
-# Define the PythonOperator task
-spark_task = PythonOperator(
-    task_id='run_spark_script',
-    python_callable=Start,
-    dag=dag,
-)
+def start_dag():
+    
+    @task
+    def _start():
+        Start()
+        
+    _start()
+
+dag = start_dag()
